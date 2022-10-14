c434df7f0a72d1d785ce23a6ba577d3d6fa946c9
Felipe Gerhardt <flc.gerhardt@gmail.com>
2022-10-13 20:02:39 -0400

    Updates

--- src/containers/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as AboutMe } from './AboutMe';
-export { default as Education } from './Education';
-export { default as Technologies } from './Techonologies';
-export { default as WorkExperience } from './WorkExperience';
-export { default as ContactMe } from './ContactMe';
-export { default as Projects } from './Projects';
