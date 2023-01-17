b40715df60949ed5c22e4e69c13846b91340f380
Felipe Gerhardt <felipegerhardt@Felipes-MacBook-Air.local>
2023-01-17 11:58:38 -0500

    Changes on Projects Card

--- src/components/ProjectsCard/index.tsx
+++ src/components/ProjectsCard/index.tsx
@@ -1,6 +1,7 @@
-import React, {ReactElement, useEffect} from 'react';
+import React, { useState, ReactElement, useEffect } from 'react';
 import './index.css'
-import {AiOutlineCheckCircle, AiOutlineLoading3Quarters} from 'react-icons/ai'
+import { AiOutlineCheckCircle, AiOutlineLoading3Quarters } from 'react-icons/ai'
+import { IoIosArrowDown } from "react-icons/io";
 
 import Aos from 'aos'
 import 'aos/dist/aos.css';
@@ -10,35 +11,41 @@ const ProjectsCard = ({
   status,
   url,
   technologies,
-}:{
-  project:string | ReactElement;
-  status:string;
-  url:string;
+}: {
+  project: string | ReactElement;
+  status: string;
+  url: string;
   technologies: Array<string>;
 }) => {
 
-  useEffect (() => {
+  const [toggle, setToggle] = useState(true);
+  const [toggleDiv, setToggleDiv] = useState(true);
+  const [toggleContainer, setToggleContainer] = useState(true)
+  const [arrow, setArrow] = useState(false);
+  function handleClick() {
+    setToggle(!toggle)
+    setToggleDiv(!toggleDiv)
+    setToggleContainer(!toggleContainer)
+    setArrow(!arrow)
+  }
 
-    Aos.init({duration: 1000})
-    
+
+  useEffect(() => {
+
+    Aos.init({ duration: 1000 })
+
   }, [])
-  
+
   return (
-    <div className='single-project-card-container' onClick={ () => window.open(url) }>
-      <div className='card-info'>
-        
-        <p className='project-title'> {project}</p>
+    <div onClick={handleClick} className={toggleDiv ? 'single-project-card-container' : 'single-project-card-container activated'} /* onClick={ () => window.open(url) } */>
+      <div className={toggle ? 'card-info' : 'card-info activated'}>
 
-        <div className="contentContainer">
-            <p className='technologies-title'>Technologies:</p>
+        <div className='title-arrow-div'>
+          <p className={toggleContainer ? 'project-title' : 'project-title activated'}> {project}</p>
 
-          <div className="description">
-
-            {technologies.map((tech) => <p className='technologies'>{tech}</p>)}
-
-          </div>
-
+          <IoIosArrowDown className={arrow ? 'arrow activated' : 'arrow'} size={15}/>
         </div>
+
       </div>
     </div>
 
